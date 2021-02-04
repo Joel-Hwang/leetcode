60c2b8f342a489cc06f1cb25f6fd778de9aced8f
unknown <gavin.c.hwang@gmail.com>
2021-02-04 22:10:08 +0900

    Parentheses

---
+++ java/Parentheses.java
@@ -0,0 +1,58 @@
+public class Parentheses {
+
+
+    public boolean isValid(String s) {
+      Stack<Character> stk = new Stack<>();
+    
+      for(int i = 0; i<s.length(); i++){
+          char cPop;
+          switch(s.charAt(i)){
+              case '(':
+              case '[':
+              case '{':
+                  stk.push(s.charAt(i));
+                      
+                  break;
+              case ')':
+                  if(stk.size()==0) return false;
+                  cPop = stk.pop();
+                  if(cPop != '(') return false;
+                  break;
+              case ']':
+                  if(stk.size()==0) return false;
+                  cPop = stk.pop();
+                  if(cPop != '[') return false;
+                  break;
+              case '}':
+                  if(stk.size()==0) return false;
+                  cPop = stk.pop();
+                  if(cPop != '{') return false;
+                  break;
+          }
+      }
+        
+      if(stk.size()>0) return false;
+      return true;
+        
+    }
+}
+
+
+/*
+
+public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+}
+
+*/
