14f13d9655276ffcb328c5dffd862f29d7e80454
Joel Hwang <joel.c.hwang@gmail.com>
2021-02-05 22:54:53 +0900

    3sum closest

---
+++ java/ThreeSumClosest.java
@@ -0,0 +1,28 @@
+class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        int diff = nums[0] +  nums[1] +  nums[2];
+        Arrays.sort(nums);
+        
+        for(int i = 0; i<nums.length-2;i++ ){
+            int l = i+1, r=nums.length-1;
+            while(l<r){
+                int curSum = nums[l]+nums[r]+nums[i];
+                if(curSum < target){
+                    if( Math.abs(curSum - target) < Math.abs(diff-target) ){
+                        diff = curSum;
+                    }
+                    l++;
+                }else if(curSum > target){
+                    if( Math.abs(curSum - target) < Math.abs(diff-target) ){
+                        diff = curSum;
+                    }
+                    r--;
+                }else{
+                    return curSum;
+                }
+            }
+        }
+       return diff;
+    }
+                          
+}
