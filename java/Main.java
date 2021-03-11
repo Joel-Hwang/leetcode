ddcd1a12d768dccb743d954fa2147ce91f9f4d51
Joel Hwang <joel.c.hwang@gmail.com>
2021-03-11 11:46:19 +0900

    _567_Permutation_in_String  5percent

--- java/Main.java
+++ java/Main.java
@@ -6,18 +6,8 @@ import Hard.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedListNode lln = new LinkedListNode();
-        
-        LinkedListNode.Node l1 = new LinkedListNode.Node(9);
-        l1.next = new LinkedListNode.Node(1);
-        l1.next.next = new LinkedListNode.Node(4);
-
-        
-        LinkedListNode.Node l2 = new LinkedListNode.Node(6);
-        l2.next = new LinkedListNode.Node(4);
-
-
-        lln.addReverse2(l1, l2,0);
+        new _567_Permutation_in_String();
     }
+
     
 }
---
+++ java/Medium/_567_Permutation_in_String.java
@@ -0,0 +1,40 @@
+package Medium;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _567_Permutation_in_String {
+    /*
+    
+    Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+ 
+"ab"
+"eidboaoo"
+    */
+    public _567_Permutation_in_String(){
+        System.out.println(checkInclusion("ab","eidboaoo"));
+    }
+
+    public boolean checkInclusion(String s1, String s2) {
+        char[] cs1 = s1.toCharArray();
+        Arrays.sort(cs1);
+        s1 = new String(cs1);
+
+        for(int i = 0; i<=s2.length()-s1.length(); i++){
+            String tempS2 = s2.substring(i, i+s1.length());
+            char[] cs2 = tempS2.toCharArray();
+            Arrays.sort(cs2);
+            tempS2 = new String(cs2);
+            if(tempS2.equals(s1)) return true;
+        }
+        return false;
+    }
+
+   
+}
