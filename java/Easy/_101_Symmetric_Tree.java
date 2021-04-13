9fe9e679281229b8d21d3ed5ec6fd900e5f12def
Joel Hwang <joel.c.hwang@gmail.com>
2021-04-14 07:39:19 +0900

    101 Symmetric Tree. Recursive and queue

--- java/Easy/_101_Symmetric_Tree.java
+++ java/Easy/_101_Symmetric_Tree.java
@@ -18,26 +18,42 @@ public class _101_Symmetric_Tree {
     
 
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        LinkedList<Integer> l = new LinkedList<>();
-        LinkedList<Integer> r = new LinkedList<>();
-        q.offer(root);
-        int cnt = 0;
-        while(!q.isEmpty()){
-            cnt++;
-            TreeNode node = q.poll();
-            if(node == null) continue;
-            System.out.println(node.val);
-            q.offer(node.left);
-            q.offer(node.right);
+        Queue<TreeNode> l = new LinkedList<>();
+        Queue<TreeNode> r = new LinkedList<>();
+        l.offer(root);
+        r.offer(root);
+        while(!l.isEmpty() && !r.isEmpty()){
+            
+            TreeNode nl = l.poll();
+            TreeNode nr = r.poll();
+            
+            if(nl == null && nr == null) continue;
+            if(nl == null && nr != null) return false;
+            if(nl != null && nr == null) return false;
+            if(nl.val != nr.val) return false;
 
+            l.offer(nl.left);
+            l.offer(nl.right);
+            r.offer(nr.right);
+            r.offer(nr.left);
         }
 
         return true;
     }
+
+    public boolean isSymmetric2(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode l, TreeNode r){
+        if(l == null && r == null) return true;
+        if(l == null || r == null) return false;
+        if(l.val != r.val) return false;
+
+        return (l.val == r.val) 
+        && isMirror(l.left, r.right) 
+        && isMirror(l.right, r.left);
+    }
 }
-
-
 
 
 class TreeNode {
