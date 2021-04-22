c7d03e90539daa71d331c945e654c711f4fdda9d
Joel Hwang <joel.c.hwang@gmail.com>
2021-04-22 09:12:25 +0900

    _679_24Game

---
+++ java/Hard/_679_24Game.java
@@ -0,0 +1,10 @@
+
+public class _679_24Game{
+    public _679_24Game(){
+
+    }
+
+    public boolean is24(int[] nums){
+        return true;
+    }
+}
--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,7 @@ import Medium._838_PushDominoes;
 
 public class Main{
     public static void main(String[] args) {
-        new _838_PushDominoes();
+        new _679_24Game();
     }
     
 }
