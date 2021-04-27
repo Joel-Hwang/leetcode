10bf513c51b8c40d9a57bd3be0659fe11b3dba9a
Joel Hwang <joel.c.hwang@gmail.com>
2021-04-27 23:26:47 +0900

    _35_Search_Insert_Position

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,8 @@
-import Medium._34_FindFirstAndLastPositionElement;
+import Easy._35_Search_Insert_Position;
 
 public class Main{
     public static void main(String[] args) {
-        new _34_FindFirstAndLastPositionElement();
+        new _35_Search_Insert_Position();
     }
     
 }
