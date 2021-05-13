00ac9d8240309a6ba9d8ad40bb85ae7d602f5c32
Joel Hwang <joel.c.hwang@gmail.com>
2021-05-14 08:07:45 +0900

    restore IP Address DFS

--- java/Medium/_93_RestoreIPAddresses.java
+++ java/Medium/_93_RestoreIPAddresses.java
@@ -1,5 +1,6 @@
 package Medium;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -45,6 +46,26 @@ public class _93_RestoreIPAddresses {
         if(s.length()>1 && s.startsWith("0")) return false;
         if("".equals(s) || Integer.parseInt(s)>255) return false;
         return true;
+    }
+
+
+    /*DFS*/
+    public List<String> restoreIpAddresses2(String s) {
+        List<String> solutions = new ArrayList<String>();
+        restoreIp(s, solutions, 0, "", 0);
+        return solutions;
+    }
+    
+    private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4) return;
+        if (count == 4 && idx == ip.length()) solutions.add(restored);
+        
+        for (int i=1; i<4; i++) {
+            if (idx+i > ip.length()) break;
+            String s = ip.substring(idx,idx+i);
+            if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+            restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        }
     }
 
 }
