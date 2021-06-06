c79bd43c6f354e8dee87916e5c87e68b9765706c
Joel Hwang <joel.c.hwang@gmail.com>
2021-06-06 23:00:05 +0900

    _39_CombinationSum

--- java/Medium/_39_CombinationSum.java
+++ java/Medium/_39_CombinationSum.java
@@ -8,48 +8,28 @@ import org.testng.annotations.Test;
 public class _39_CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-
-        List<Integer> cands = new ArrayList<Integer>();
-        for(int cand : candidates){
-            int div = target/cand;
-            for(int i = 1; i<=div; i++){
-                cands.add(cand);
-            }
-        }
-
-        combination(cands,0,1,"",target,res);
-        combination(cands,0,2,"",target,res);
-        combination(cands,0,3,"",target,res);
-        combination(cands,0,4,"",target,res);
-
+        combination(candidates,0,new ArrayList<Integer>() ,target,res);
         return res;
     }
 
-    public void combination(List<Integer> ar,int start, int pick, String parent,int target, List<List<Integer>> res){
-        if( pick < 1){
-            int sum = 0;
-            for(char c : parent.toCharArray()){
-                sum += (c-'0');
-            }
-            
-            if (sum == target) {
-                List<Integer> a = new ArrayList();
-                for(char c : parent.toCharArray()){
-                    a.add(c-'0');
-                }
-                res.add(a);
-            }
+    public void combination(int[] cands,int start, List<Integer> comb, int target, List<List<Integer>> res){
+        if( target == 0){
+            res.add(new ArrayList<>(comb));
             return;
         }
        
-        for(int i = start; i<ar.size()-(pick-1); i++){
-            combination(ar,i+1, pick-1, parent+ar.get(i), target, res);
+        for(int i = start; i<cands.length; i++){
+            if(cands[i]>target) continue;
+            comb.add(cands[i]);
+            combination(cands,i, comb, target-cands[i], res);
+            //DFS에서 마지막 요소를 제거해야 부모로 올라간 후 다음 자식으로 내려옴
+            comb.remove(comb.size()-1);
         }
     }
 
     @Test
     public void test(){
-        combinationSum(new int[]{2,3,6,7},5);
+        combinationSum(new int[]{2,3,6,7},7);
 
     }
 }
