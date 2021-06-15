91a5d38841027fa2428e238595be506a42fcee06
Joel Hwang <joel.c.hwang@gmail.com>
2021-06-16 08:05:53 +0900

    _665_NonDecreasingArray

--- java/Medium/_665_NonDecreasingArray.java
+++ java/Medium/_665_NonDecreasingArray.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 import org.testng.annotations.Test;
 
 public class _665_NonDecreasingArray {
@@ -19,20 +20,42 @@ public class _665_NonDecreasingArray {
         
         */
         for(int i = 1; i<nums.length; i++){
-            if(i<1) continue;
-            if(nums[i] <nums[i-1]){
-                nums[i-1] = nums[i];
+            if(nums[i] >=nums[i-1]) continue;
+            int next = i==nums.length-1?Math.max(nums[i],nums[i-1]):nums[i+1];
+            int cur = Math.max(next, nums[i-1]);
+
+            if(nums[i-1]<=cur && cur<=next){
                 cnt++;
-                i-=2;
+                continue;
             }
+            int prev = Math.max(nums[i], nums[i<2?i:i-2]);
+            if(nums[i<2?i:i-2]<=prev && prev<=nums[i]){
+                cnt++;
+                continue;
+            }
+            return false;
         }
         return cnt<2;
     }
+
+    public boolean checkPossibility2(int[] nums) {
+        int cnt = 0;    //the number of changes
+        for(int i = 1; i < nums.length && cnt<=1 ; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];  //modify nums[i-1] of a priority
+                else nums[i] = nums[i-1];   //have to modify nums[i]
+            }
+        }
+        return cnt<=1; 
+    }
+
     @Test
     public void test(){
         assertTrue(checkPossibility(new int[]{4,2,3}));
         assertFalse(checkPossibility(new int[]{4,2,1}));
         assertFalse(checkPossibility(new int[]{3,4,2,3}));
-        assertFalse(checkPossibility(new int[]{5,7,1,8}));
+        assertTrue(checkPossibility(new int[]{5,7,1,8}));
+        assertTrue(checkPossibility(new int[]{1,2,4,5,3}));
     }
 }
