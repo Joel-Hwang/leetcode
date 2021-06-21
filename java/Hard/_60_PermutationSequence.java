69e7ea56a84b9273210c1afc0509dd4a6785e64b
Joel Hwang <joel.c.hwang@gmail.com>
2021-06-21 09:53:41 +0900

    _60_PermutationSequence

--- java/Hard/_60_PermutationSequence.java
+++ java/Hard/_60_PermutationSequence.java
@@ -1,5 +1,8 @@
 package Hard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class _60_PermutationSequence {
@@ -48,6 +51,38 @@ public class _60_PermutationSequence {
         }
     }
 
+    public String getPermutation2String(int n, int k) {
+        int pos = 0;
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n+1];
+        StringBuilder sb = new StringBuilder();
+        
+        // create an array of factorial lookup
+        int sum = 1;
+        factorial[0] = 1;
+        for(int i=1; i<=n; i++){
+            sum *= i;
+            factorial[i] = sum;
+        }
+        // factorial[] = {1, 1, 2, 6, 24, ... n!}
+        
+        // create a list of numbers to get indices
+        for(int i=1; i<=n; i++){
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+        
+        k--;
+        
+        for(int i = 1; i <= n; i++){
+            int index = k/factorial[n-i];
+            sb.append(String.valueOf(numbers.get(index)));
+            numbers.remove(index);
+            k-=index*factorial[n-i];
+        }
+        
+        return String.valueOf(sb);
+    }
 
     @Test
     public void test(){
