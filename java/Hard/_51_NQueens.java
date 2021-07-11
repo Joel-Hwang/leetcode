d19a81e686a497c1b8d635a2028e1c151c8bae49
Joel Hwang <joel.c.hwang@gmail.com>
2021-07-12 08:43:43 +0900

    _51_NQueens need distinct

---
+++ java/Hard/_51_NQueens.java
@@ -0,0 +1,79 @@
+package Hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class _51_NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        String[][] map = new String[n][n];
+        solve(map, n, res);
+
+        return res;
+    }
+
+    public void solve(String[][] map, int n, List<List<String>> res) {
+        
+        if (n == 0) {
+            ArrayList<String> cs = new ArrayList<String>();
+            for (int i = 0; i < map.length; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < map[i].length; j++) {
+                    sb.append(map[i][j]);
+                }
+                cs.add(sb.toString());
+            }
+            res.add(cs);
+            return;
+        }
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (setQ(map, i, j, ""+n)) {
+                    solve(map, n - 1, res);
+                    unset(map,i,j,""+n);
+                }
+                
+            }
+        }
+    }
+
+    public boolean setQ(String[][] map, int y, int x, String n) {
+        boolean res = true;
+        if (map[y][x] != null)
+            return false;
+
+        map[y][x] = "Q";
+
+        for (int i = 0; i < map.length; i++) {
+            if (map[y][i] == null)
+                map[y][i] = n;
+            if (map[i][x] == null)
+                map[i][x] = n;
+
+            if (y - i >= 0 && x - i >= 0 && map[y - i][x - i] == null)
+                map[y - i][x - i] = n;
+            if (y + i < map.length && x + i < map.length && map[y + i][x + i] == null)
+                map[y + i][x + i] = n;
+            if (y + i < map.length && x - i >=0 && map[y + i][x - i] == null)
+                map[y + i][x - i] = n;
+            if (y - i >= 0 && x + i < map.length && map[y - i][x + i] == null)
+                map[y - i][x + i] = n;
+        }
+
+        return res;
+    }
+
+    public void unset(String[][] map, int y, int x, String n){
+        map[y][x] = null;
+        for(int i = 0; i<map.length; i++)
+            for(int j = 0; j< map[i].length; j++)
+                if(n.equals(map[i][j])) map[i][j] = null;
+    }
+
+    @Test
+    public void test() {
+        solveNQueens(4);
+    }
+}
