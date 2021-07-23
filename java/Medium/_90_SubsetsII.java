fde22557e7ea74cd16d642c859e8e36a213da7d1
Joel Hwang <joel.c.hwang@gmail.com>
2021-07-24 07:34:40 +0900

    _90_SubsetsII

---
+++ java/Medium/_90_SubsetsII.java
@@ -0,0 +1,66 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class _90_SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int cnt = 0; cnt<=nums.length; cnt++){
+            comb(nums,0,cnt,new ArrayList<>(), res);
+        }
+        
+        return res;
+    }  
+
+    public void comb(int[] nums, int start, int cnt,List<Integer> prt, List<List<Integer>> res){
+        if(cnt == 0){
+            res.add(new ArrayList<>(prt));
+            return;
+        }
+        
+        for(int i = start; i<nums.length-(cnt-1); i++){
+            if(i>start && nums[i] == nums[i-1]) continue;
+            prt.add(nums[i]);
+            comb(nums,i+1,cnt-1,prt,res);
+            prt.remove(prt.size()-1);
+        }
+
+    }
+
+    //Back Tracking
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> currentSubset = new ArrayList<>();
+
+        subsetsWithDupHelper(subsets, currentSubset, nums, 0);
+        return subsets;
+    }
+
+    private void subsetsWithDupHelper(List<List<Integer>> subsets, List<Integer> currentSubset, int[] nums, int index) {
+        // Add the subset formed so far to the subsets list.
+        subsets.add(new ArrayList<>(currentSubset));
+
+        for (int i = index; i < nums.length; i++) {
+            // If the current element is a duplicate, ignore.
+            if (i != index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            currentSubset.add(nums[i]);
+            subsetsWithDupHelper(subsets, currentSubset, nums, i + 1);
+            currentSubset.remove(currentSubset.size() - 1);
+        }
+    }
+
+    @Test
+    public void test(){
+        subsetsWithDup(new int[]{1,2,3});
+        subsetsWithDup(new int[]{1,2,2});
+        subsetsWithDup(new int[]{0});
+    }
+}
