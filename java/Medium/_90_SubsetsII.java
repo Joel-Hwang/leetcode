5eb307a9dcbed8f8813ff9b7780bcd8cbb184b09
Joel Hwang <joel.c.hwang@gmail.com>
2021-07-24 07:36:50 +0900

    _90_SubsetsII BackTracking

--- java/Medium/_90_SubsetsII.java
+++ java/Medium/_90_SubsetsII.java
@@ -10,25 +10,23 @@ public class _90_SubsetsII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for(int cnt = 0; cnt<=nums.length; cnt++){
-            comb(nums,0,cnt,new ArrayList<>(), res);
-        }
+        //for(int cnt = 0; cnt<=nums.length; cnt++){
+            comb(nums,0,new ArrayList<>(), res);
+        //}
         
         return res;
     }  
 
-    public void comb(int[] nums, int start, int cnt,List<Integer> prt, List<List<Integer>> res){
-        if(cnt == 0){
-            res.add(new ArrayList<>(prt));
-            return;
-        }
+    public void comb(int[] nums, int start,List<Integer> prt, List<List<Integer>> res){
+        res.add(new ArrayList<>(prt));
         
-        for(int i = start; i<nums.length-(cnt-1); i++){
+        for(int i = start; i<nums.length; i++){
             if(i>start && nums[i] == nums[i-1]) continue;
             prt.add(nums[i]);
-            comb(nums,i+1,cnt-1,prt,res);
+            comb(nums,i+1,prt,res);
             prt.remove(prt.size()-1);
         }
+        return;
 
     }
 
