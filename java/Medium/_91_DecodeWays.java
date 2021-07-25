12cc4fba4296e02cb3578104efd844079fc22f41
Joel Hwang <joel.c.hwang@gmail.com>
2021-07-25 13:15:39 +0900

    _91_DecodeWays

--- java/Medium/_91_DecodeWays.java
+++ java/Medium/_91_DecodeWays.java
@@ -3,15 +3,16 @@ package Medium;
 import org.testng.annotations.Test;
 
 public class _91_DecodeWays {
+    //DP
     public int numDecodings(String s) {
         if(s.startsWith("0")) return 0;
         int[] dp = new int[s.length()+1];
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2; i<s.length()+1; i++){
-            if(s.charAt(i-1) == '0' && s.charAt(i-2)>'2' ) return 0;
+            if(s.charAt(i-1) == '0' && (s.charAt(i-2)>'2' || s.charAt(i-2)=='0') ) return 0;
 
-            if(s.charAt(i-1) == '0' ) dp[i] = dp[i-1];
+            if(s.charAt(i-1) == '0' ) dp[i] = dp[i-2];
             else if(isConvertable(s.charAt(i-2), s.charAt(i-1)))
                 dp[i] = dp[i-1]+dp[i-2];
             else dp[i] = dp[i-1];
@@ -27,6 +28,7 @@ public class _91_DecodeWays {
 
     @Test
     public void test(){
+        System.out.println(numDecodings("10011"));
         System.out.println(numDecodings("2101"));
         System.out.println(numDecodings("10"));
         System.out.println(numDecodings("12"));
