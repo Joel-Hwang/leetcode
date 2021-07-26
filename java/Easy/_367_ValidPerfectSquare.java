2ae60dcd54a270b38f5e7a6572c846f302d5db2f
Joel Hwang <joel.c.hwang@gmail.com>
2021-07-27 08:57:12 +0900

    _367_ValidPerfectSquare binary search

--- java/Easy/_367_ValidPerfectSquare.java
+++ java/Easy/_367_ValidPerfectSquare.java
@@ -3,8 +3,19 @@ package Easy;
 import org.testng.annotations.Test;
 
 public class _367_ValidPerfectSquare {
-
     public boolean isPerfectSquare(int num) {
+        if(num == 1 || num == 4 || num == 9) return true;
+        int l = 1, r = num/2;
+        while(l<r){
+            long m = (l+r)/2;
+            if(m*m == num) return true;
+            else if(m*m>num) r = (int)m;
+            else l = (int)m+1;
+        }   
+        return false;
+    }
+
+    public boolean isPerfectSquare2(int num) {
         if(num == 1) return true;
         int power2 = 0;
         for(int i = 1; i<=num/2; i++){
@@ -19,6 +30,8 @@ public class _367_ValidPerfectSquare {
 
     @Test
     public void test(){
+        System.out.println(isPerfectSquare(808201));
+        System.out.println(isPerfectSquare(4));
         System.out.println(isPerfectSquare(14));
         System.out.println(isPerfectSquare(2147483647));
         System.out.println(isPerfectSquare(1));
