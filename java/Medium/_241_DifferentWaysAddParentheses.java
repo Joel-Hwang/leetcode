052c3912d04781e6da3333f451cd8f528424fd21
Joel Hwang <joel.c.hwang@gmail.com>
2021-08-04 08:43:39 +0900

    _241_DifferentWaysAddParentheses

--- java/Medium/_241_DifferentWaysAddParentheses.java
+++ java/Medium/_241_DifferentWaysAddParentheses.java
@@ -10,38 +10,38 @@ import org.testng.annotations.Test;
 
 
 public class _241_DifferentWaysAddParentheses {
-    Map<String, List<Integer>> map = new HashMap<>();
+    Map<String,List<Integer>> map = new HashMap<>();
     public List<Integer> diffWaysToCompute(String input) {
         if(map.containsKey(input)) return map.get(input);
-        List<Integer> ret = new ArrayList<>();
-        for(int i = 0; i<input.length()-1; i++){
+        List<Integer> ret = new ArrayList<Integer>();
+
+        for(int i = 0; i<input.length(); i++){
             if(input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*'){
-                String part1 = input.substring(0, i);
-                String part2 = input.substring(i+1);
-                List<Integer> part1Ret = diffWaysToCompute(part1);
-                List<Integer> part2Ret = diffWaysToCompute(part2);
-                for(Integer p1 : part1Ret){
-                    for(Integer p2 : part2Ret){
-                        int curRes = 0;
+                String sp1 = input.substring(0, i);
+                String sp2 = input.substring(i+1);
+                List<Integer> lP1 = diffWaysToCompute(sp1);
+                List<Integer> lP2 = diffWaysToCompute(sp2);
+                for(Integer p1 : lP1)
+                    for(Integer p2 : lP2){
+                        int cRes = 0;
                         switch(input.charAt(i)){
                             case '+':
-                            curRes = p1+p2;
+                            cRes = p1+p2;
                             break;
                             case '-':
-                            curRes = p1-p2;
+                            cRes = p1-p2;
                             break;
                             case '*':
-                            curRes = p1*p2;
+                            cRes = p1*p2;
                             break;
                         }
-                        ret.add(curRes);
+                        ret.add(cRes);
                     }
-                }
             }
         }
-        if(ret.size() == 0)
-            ret.add(Integer.parseInt(input));
 
+        if(ret.size() == 0) 
+            ret.add(Integer.parseInt(input));
         map.put(input, ret);
         return ret;
     }
