a0898c2a95afe9ec227ca9b236c6f57cb8529f85
Joel Hwang <joel.c.hwang@gmail.com>
2021-08-19 22:00:01 +0900

    _1339_MaximumProductOfSplittedBinaryTree

--- java/Medium/_1339_MaximumProductOfSplittedBinaryTree.java
+++ java/Medium/_1339_MaximumProductOfSplittedBinaryTree.java
@@ -29,7 +29,20 @@ public class _1339_MaximumProductOfSplittedBinaryTree {
         return subTotal;
     }
 
-    
+    /*
+    long res = 0, total = 0, sub;
+    public int maxProduct(TreeNode root) {
+        total = s(root); s(root);
+        return (int)(res % (int)(1e9 + 7));
+    }
+
+    private long s(TreeNode root) {
+        if (root == null) return 0;
+        sub = root.val + s(root.left) + s(root.right);
+        res = Math.max(res, sub * (total - sub));
+        return sub;
+    }
+    */
 
 
     @Test
