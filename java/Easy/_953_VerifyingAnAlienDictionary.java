91aaff6099ccdc9e90f10be3e02cb44ff33a6119
Joel Hwang <joel.c.hwang@gmail.com>
2021-09-14 07:44:40 +0900

    _953_VerifyingAnAlienDictionary

--- java/Easy/_953_VerifyingAnAlienDictionary.java
+++ java/Easy/_953_VerifyingAnAlienDictionary.java
@@ -1,9 +1,14 @@
 package Easy;
 
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 public class _953_VerifyingAnAlienDictionary {
-    int[] dic = new int[26];
+    //Use Array for dictionary takes 0ms (100%)
+    //Use HashMap for dictionary takes 1ms(54.85%)
+    //spacity is same.
+    /*int[] dic = new int[26];
     public boolean isAlienSorted(String[] words, String order) {
         for(int i = 0; i<order.length(); i++){
             dic[order.charAt(i) - 'a'] = i;
@@ -26,7 +31,30 @@ public class _953_VerifyingAnAlienDictionary {
 
         return true;
     }
+*/
+    HashMap<Character, Integer> dic = new HashMap<>();
+    public boolean isAlienSorted(String[] words, String order) {
+        for(int i = 0; i<order.length(); i++){
+            dic.put(order.charAt(i), i);
+        }
 
+        for(int i = 1; i<words.length; i++){
+            if(!compare(words[i-1], words[i])) return false;
+        }
+        return true;
+    }
+
+    private boolean compare(String a, String b) {
+        for(int i = 0; i<Math.min(a.length(), b.length()); i++){
+            
+            if(dic.get(a.charAt(i)) < dic.get(b.charAt(i))) return true;
+            if(dic.get(a.charAt(i)) > dic.get(b.charAt(i))) return false;
+        }
+        if(a.length() > b.length())
+            return false;
+
+        return true;
+    }
     @Test
     public void test(){
         System.out.println(isAlienSorted(new String[]{"hello","leetcode"}, "hlabcdefgijkmnopqrstuvwxyz"));
