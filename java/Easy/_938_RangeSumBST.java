eb16a50d02a448d24040ec9808801f63e1c6e1c2
Joel Hwang <joel.c.hwang@gmail.com>
2021-09-20 07:59:42 +0900

    _938_RangeSumBST

--- java/Easy/_938_RangeSumBST.java
+++ java/Easy/_938_RangeSumBST.java
@@ -8,12 +8,12 @@ public class _938_RangeSumBST {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null)
             return 0;
-        else if (root.val < low) {
+        if (root.val < low)
             return rangeSumBST(root.right, low, high);
-        } else if (root.val > high) {
+        if (root.val > high)
             return rangeSumBST(root.left, low, high);
-        } else
-            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
 
     }
 }
