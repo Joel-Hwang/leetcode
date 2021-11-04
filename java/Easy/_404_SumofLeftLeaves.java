97549719f1a6b5da9ed437cf529d5864c425fa72
Joel Hwang <joel.c.hwang@gmail.com>
2021-11-05 07:52:02 +0900

    _404_SumofLeftLeaves

---
+++ java/Easy/_404_SumofLeftLeaves.java
@@ -0,0 +1,18 @@
+package Easy;
+
+import CS.TreeNode;
+
+public class _404_SumofLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null ) return 0;
+        if(root.left == null && root.right == null) return 0;
+        return rec(root.left,true) + rec(root.right,false);
+    }
+
+    private int rec(TreeNode node, boolean isL){
+        if(node == null) return 0;
+        if( isL && node.left == null && node.right == null) return node.val;
+
+        return rec(node.left,true) + rec(node.right,false);
+    }
+}
