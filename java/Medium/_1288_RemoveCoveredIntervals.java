7bd82083a362d01e4ec1ab52a8d26a9c67666cb2
Joel Hwang <66057807+Joel-Hwang@users.noreply.github.com>
2022-02-21 07:54:45 +0900

    comparator

--- java/Medium/_1288_RemoveCoveredIntervals.java
+++ java/Medium/_1288_RemoveCoveredIntervals.java
@@ -6,11 +6,7 @@ import org.testng.annotations.Test;
 
 public class _1288_RemoveCoveredIntervals {
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (o1, o2) -> {
-            if(o1[0] == o2[0]) return o2[1] - o1[1];
-            return o1[0] - o2[0];
-        });
-
+        Arrays.sort(intervals, (o1, o2) -> o1[0] == o2[0]?o2[1] - o1[1]:o1[0] - o2[0]);
         int res = intervals.length;
         for(int i = 1; i<intervals.length; i++){
             if(isIn(intervals[i-1], intervals[i]))
