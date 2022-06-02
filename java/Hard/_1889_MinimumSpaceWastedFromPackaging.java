28f69dbe7553b19814e569bdd441f116fd5c107e
Joel Hwang <66057807+Joel-Hwang@users.noreply.github.com>
2022-06-02 11:15:42 +0900

    _1889_MinimumSpaceWastedFromPackaging

--- java/Hard/_1889_MinimumSpaceWastedFromPackaging.java
+++ java/Hard/_1889_MinimumSpaceWastedFromPackaging.java
@@ -7,47 +7,113 @@ import java.util.Arrays;
 import org.testng.annotations.Test;
 
 public class _1889_MinimumSpaceWastedFromPackaging {
-    public int minWastedSpace(int[] packages, int[][] boxes) {
-        long res = -1;
-        sortAll(packages, boxes);
 
-        for(int[] box : boxes){
-            long curSpace = getWastedSpace(packages, box);
-            if(curSpace < 0) continue;
-            res = res<0?curSpace:Math.min(res,curSpace);
+    public int minWastedSpace(int[] pkg, int[][] boxes) {
+        long inf = (long) 1e11, res = inf, mod = (long) 1e9 + 7, sumA = 0L;
+        Arrays.sort(pkg);
+        for (int pck : pkg)
+            sumA += pck;
+
+        for (int[] box : boxes) {
+            Arrays.sort(box);
+            if (box[box.length - 1] < pkg[pkg.length - 1]) continue;
+
+            long curSpace = getWastedSpace(pkg, box);
+            res = Math.min(res, curSpace);
         }
-        return (int)(res%1000000007);
+        return res < inf ? (int) ((res - sumA) % mod) : -1;
     }
 
-    private void sortAll(int[] packages, int[][] boxes){
-        Arrays.sort(packages);
-
-        for(int[] box : boxes) Arrays.sort(box);
-
-        Arrays.sort(boxes,(a,b) -> {
-            return a[a.length-1] - b[b.length-1];
-        });
-    }
-    
-    private long getWastedSpace(int[] packages, int[] boxes){
-        if(packages[packages.length-1] > boxes[boxes.length-1]) return -1;
-        long res = 0;
-        int pkgCursor = 0;
-        int boxCursor = 0;
-        while(pkgCursor < packages.length && boxCursor < boxes.length){
-            if(packages[pkgCursor] <= boxes[boxCursor]){
-                res += boxes[boxCursor] - packages[pkgCursor];
-                pkgCursor++;
-            }else{
-                boxCursor++;
+    private long getWastedSpace(int[] pkgs, int[] box) {
+        long cur = 0;
+        long i = 0;
+        for (int b : box) {
+            int l = 0, r = pkgs.length;
+            while (l < r) {
+                int m = (l + r) / 2;
+                if (pkgs[m] < b + 1)
+                    l = m + 1;
+                else
+                    r = m;
             }
-            
-        } 
-        return res;
+
+            cur += b * (l - i);
+            i = l;
+        }
+        return cur;
+    }
+
+    public int minWastedSpace3(int[] pkg, int[][] boxes) {
+        Arrays.sort(pkg);
+        long inf = (long) 1e11, res = inf, mod = (long) 1e9 + 7, sumA = 0L;
+        for (int a : pkg)
+            sumA += a;
+
+        for (int[] box : boxes) {
+            Arrays.sort(box);
+            if (box[box.length - 1] < pkg[pkg.length - 1]) continue;
+            long cur = 0, i = 0;
+            for (int b : box) {
+                int l = 0, r = pkg.length;
+                while (l < r) {
+                    int m = (l + r) / 2;
+                    if (pkg[m] < b+1)
+                        l = m + 1;
+                    else
+                        r = m;
+                }
+                cur += b * (l - i);
+                i = l;
+            }
+            res = Math.min(res, cur);
+        }
+        return res < inf ? (int) ((res - sumA) % mod) : -1;
+    }
+
+    public int minWastedSpace2(int[] pkg, int[][] boxes) {
+        Arrays.sort(pkg);
+        long inf = (long) 1e11, res = inf, mod = (long) 1e9 + 7, sumA = 0L;
+        for (int a : pkg)
+            sumA += a;
+        for (int[] box : boxes) {
+            Arrays.sort(box);
+            if (box[box.length - 1] < pkg[pkg.length - 1]) continue;
+            long cur = 0, i = 0; int j;
+            for (int b : box) {
+                j = binarySearch(pkg, b + 1);
+                cur += b * (j - i);
+                i = j;
+            }
+            res = Math.min(res, cur);
+        }
+        return res < inf ? (int) ((res - sumA) % mod) : -1;
+    }
+
+    private int binarySearch(int[] pkg, int b) {
+        int l = 0, r = pkg.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (pkg[m] < b)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
     }
 
     @Test
-    public void test(){
-        assertEquals(6,minWastedSpace(new int[]{2,3,5}, new int[][]{{4,8},{2,8}}));
+    public void test() {
+        int[] pkg = new int[100000];
+        for(int i = 0; i<pkg.length; i++) pkg[i] = (i+1);
+        int[][] boxes = new int[50000][2];
+        for(int i = 0; i<boxes.length; i++){
+            boxes[i][0] = 50000 + i;
+            boxes[i][1] = 100000;
+
+        }
+
+        assertEquals(499949986, minWastedSpace(pkg, boxes ));
+        //assertEquals(-1, minWastedSpace3(new int[] { 2, 3, 5 }, new int[][] { { 1, 4 }, { 2, 3 }, { 3, 4 } }));
+        //assertEquals(6, minWastedSpace(new int[] { 2, 3, 5 }, new int[][] { { 4, 8 }, { 2, 8 } }));
     }
 }
