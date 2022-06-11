5654418bb05501647eb7922f56512bb321e3d4dc
Joel Hwang <joel.c.hwang@gmail.com>
2022-06-11 17:02:53 +0900

    _1658_MinimumOperationsToReduceXtoZero

--- java/Medium/_1658_MinimumOperationsToReduceXtoZero.java
+++ java/Medium/_1658_MinimumOperationsToReduceXtoZero.java
@@ -32,6 +32,24 @@ public class _1658_MinimumOperationsToReduceXtoZero {
         return res==100001?-1:res;
     }
 
+    public int minOperations2(int[] nums, int x) {
+        int target = -x;
+        for(int num: nums) target += num;
+        if(target == 0) return nums.length;
+        if(target < 0) return -1;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0, L = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            L = Math.max(L, i - map.getOrDefault(sum - target, i));
+            map.put(sum, i);
+        }
+        return L > 0 ? nums.length - L : -1;
+    }
+
     @Test
     public void test(){
         assertEquals(-1, minOperations(new int[]{1,1}, 3));
