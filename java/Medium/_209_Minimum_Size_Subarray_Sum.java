301e7aaa3e408dd613f92dfb34e061570f92f286
Joel Hwang <66057807+Joel-Hwang@users.noreply.github.com>
2022-08-04 10:56:08 +0900

    _209_Minimum_Size_Subarray_Sum

--- java/Medium/_209_Minimum_Size_Subarray_Sum.java
+++ java/Medium/_209_Minimum_Size_Subarray_Sum.java
@@ -1,29 +1,29 @@
 package Medium;
 
+import org.testng.annotations.Test;
+
 public class _209_Minimum_Size_Subarray_Sum {
-    public _209_Minimum_Size_Subarray_Sum() {
-        int len = minSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 });
-        System.out.println(len);
-    }
-
     public int minSubArrayLen(int target, int[] nums) {
-
-        int n = nums.length;
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += nums[k];
-                    System.out.println(String.format("i: %d, j:%d, k:%d", i,j,k));
-                }
-                if (sum >= target) {
-                    ans = Math.min(ans, (j - i + 1));
-                    break; // Found the smallest subarray with sum>=s starting with index i, hence move to
-                           // next index
-                }
+        int l = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum >= target){
+                while(sum >= target){
+                    res = Math.min(res, i-l+1);
+                    sum -= nums[l++];
+                } 
             }
         }
-        return (ans != Integer.MAX_VALUE) ? ans : 0;
+
+        return res == Integer.MAX_VALUE?0:res;
+    }
+
+
+    @Test
+    public void test(){
+        int len = minSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 });
+        System.out.println(len);
     }
 }
