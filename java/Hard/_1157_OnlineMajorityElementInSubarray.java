0ca084c412ba167648d009d9f1cf60a098f46178
Joel Hwang <joel.c.hwang@gmail.com>
2022-08-17 15:42:32 +0900

    _1157_OnlineMajorityElementInSubarray

--- java/Hard/_1157_OnlineMajorityElementInSubarray.java
+++ java/Hard/_1157_OnlineMajorityElementInSubarray.java
@@ -7,37 +7,62 @@ import org.testng.annotations.Test;
 
 public class _1157_OnlineMajorityElementInSubarray {
     class MajorityChecker {
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
-        Map<Integer, PriorityQueue<int[]>> heap = new HashMap<>();
+        private final int digits = 15;
+        private int[][] presum;
+        private ArrayList<Integer>[] pos;
         public MajorityChecker(int[] arr) {
-            map.put(0, new HashMap<Integer,Integer>());
-            heap.put(0, new PriorityQueue<int[]>((int[] a, int[] b)->{return b[1] - a[1];}));
+            int len = arr.length;
+            presum = new int[len+1][digits];
+            pos = new ArrayList[20001];
 
-            map.get(0).put(arr[0], 1);
-            heap.get(0).add(new int[]{arr[0],1});
-            
-            for(int i = 1; i<arr.length; i++){
-                map.put(i, new HashMap<Integer,Integer>(map.get(i-1)));
-                Map<Integer, Integer> rowMap = map.get(i);
-                rowMap.put(arr[i], rowMap.getOrDefault(arr[i],0)+1);
-                
-                heap.put(i, new PriorityQueue<int[]>((int[] a, int[] b)->{return b[1] - a[1];}));
-                for(int key : rowMap.keySet())
-                    heap.get(i).add(new int[]{key,rowMap.get(key)});
+            for(int i = 0; i<len; i++){
+                int n = arr[i];
+                if(pos[n] == null) pos[n] = new ArrayList<>();
+                pos[n].add(i);
+
+                for(int j = 0; j < digits; j++){
+                    //n&1은 n의 1의자리가 1이면 1을 리턴하고 아니면 0을 리턴
+                    presum[i+1][j] = presum[i][j]+(n&1);
+                    n >>= 1;
+                }
             }
+
+            // arr = [3,2,3]
+            // presum = [1,1][2,1][3,2]
+            // pos[value] = i
+            
         }
         
         public int query(int left, int right, int threshold) {
-            Map<Integer,Integer> mapLeft = map.getOrDefault(left-1,new HashMap<Integer,Integer>());
-
-            PriorityQueue<int[]> pq = new PriorityQueue<int[]>(heap.get(right));
-            while(!pq.isEmpty()){
-                int[] cur = pq.poll();
-                if(cur[1] < threshold) return -1;
-                if(cur[1] - mapLeft.getOrDefault(cur[0],0) >= threshold) return cur[0];
+            int ans = 0;
+            for(int i = digits-1; i>=0; i--){
+                int cnt = presum[right+1][i] - presum[left][i];
+                int b = 1;
+                if(cnt >= threshold) b = 1;
+                else if(right-left+1-cnt >= threshold) b = 0;
+                else return -1;
+                ans = (ans << 1) + b;
             }
 
+            //check
+            ArrayList<Integer> list = pos[ans];
+            if(list == null) return -1;
+            int L = floor(list, left-1);
+            int R = floor(list, right);
+            if(R-L >= threshold) return ans;
             return -1;
+        }
+
+        private int floor(ArrayList<Integer> list, int n){
+            int left = 0, right = list.size()-1, mid;
+            while(left<= right){
+                mid = left + (right-left)/2;
+                int index = list.get(mid);
+                if(index == n) return mid;
+                else if (index < n) left = mid+1;
+                else right = mid-1;
+            }
+            return right;
         }
 
     }
