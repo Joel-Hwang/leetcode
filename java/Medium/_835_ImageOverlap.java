10bedfc74afbbe392a644febb6c94abb8fef6238
Joel Hwang <66057807+Joel-Hwang@users.noreply.github.com>
2022-12-08 08:21:36 +0900

    _835_ImageOverlap

--- java/Medium/_835_ImageOverlap.java
+++ java/Medium/_835_ImageOverlap.java
@@ -1,9 +1,31 @@
 package Medium;
 
+import java.util.*;
+
 import org.testng.annotations.Test;
 
 public class _835_ImageOverlap {
-    public int largestOverlap(int[][] img1, int[][] img2) {
+
+    public int largestOverlap(int[][] A, int[][] B) {
+        int res = 0;
+        List<Integer> LA = new ArrayList<>();
+        List<Integer> LB = new ArrayList<>();
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int n = A.length;
+        for(int i = 0; i<n*n; i++) if(A[i/n][i%n] == 1) LA.add(i/n*100+i%n); 
+        for(int i = 0; i<n*n; i++) if(B[i/n][i%n] == 1) LB.add(i/n*100+i%n); 
+
+        for(int i = 0; i<LA.size(); i++)
+            for(int j = 0; j<LB.size(); j++)
+                count.put(LA.get(i) - LB.get(j), count.getOrDefault(LA.get(i) - LB.get(j), 0)+1);
+
+        for(int key : count.keySet())
+            res = Math.max(res, count.get(key));
+        return res;
+    }
+
+
+    public int largestOverlap2(int[][] img1, int[][] img2) {
         int res = 0;
         for(int i = -img2.length+1; i<img2.length; i++)
             for(int j = -img2[0].length+1; j<img2[0].length; j++)
