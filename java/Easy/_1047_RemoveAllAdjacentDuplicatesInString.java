ccbb05de66a9508f0444383fe2240c99387fa89b
Joel-Hwang <joel.c.hwang@gmail.com>
2023-12-16 08:42:12 -0600

    .

--- java/Easy/_1047_RemoveAllAdjacentDuplicatesInString.java
+++ java/Easy/_1047_RemoveAllAdjacentDuplicatesInString.java
@@ -4,6 +4,17 @@ import org.testng.annotations.Test;
 
 public class _1047_RemoveAllAdjacentDuplicatesInString {
     public String removeDuplicates(String s) {
+        char[] arC = s.toCharArray();
+        int i = 0;
+        for(int j = 0; j<arC.length; j++, i++){
+            arC[i] = arC[j];
+            if(i>0 && arC[i]==arC[i-1]) i-=2;
+        }
+        return new String(arC,0,i);
+        
+    }
+
+    public String removeDuplicates2(String s) {
         StringBuilder sb = new StringBuilder(s);
         for(int i = 0; i<sb.length()-1; i++) {
             if(sb.charAt(i) == sb.charAt(i+1)){
@@ -16,7 +27,7 @@ public class _1047_RemoveAllAdjacentDuplicatesInString {
 
     @Test
     public void test(){
+        removeDuplicates("azxxxzy");
         removeDuplicates("abbaca");
-        removeDuplicates("azxxzy");
     }
 }
